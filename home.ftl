18fe321a29e93453a9adcbb1446fed2cfcc5adc7
rkcerman <rkaliarik@gmail.com>
2016-07-15 22:04:10 +0200

    Implemented basic Spark & Freemarker.

---
+++ home.ftl
@@ -0,0 +1,8 @@
+<head>[BR]
+  <title>Welcome!</title>[BR]
+</head>[BR]
+<body>[BR]
+  <#-- Greet the user with his/her name -->[BR]
+  <h1>Welcome ${message}!</h1>[BR]
+</body>[BR]
+</html>
--- src/main/java/com/rk/jsonvaluefetcher/JSONValueFetcher.java
+++ src/main/java/com/rk/jsonvaluefetcher/JSONValueFetcher.java
@@ -1,20 +1,16 @@
 package main.java.com.rk.jsonvaluefetcher;
 
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.SystemUtils;
-import org.apache.commons.lang3.tuple.Pair;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.ModelAndView;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
+import static spark.Spark.*;
 
 /**
  * Created by rkcerman on 11.7.2016.
@@ -24,6 +20,18 @@ public class JSONValueFetcher extends Fetcher {
     static Logger logger = LoggerFactory.getLogger(JSONValueFetcher.class);
 
     public static void main(String[] args) {
+        FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+        Configuration freeMarkerConfiguration = new Configuration();
+        freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(JSONValueFetcher.class, "/"));
+        freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+
+        get("/jsonfetch", (req, res) -> {
+            res.status(200);
+            res.type("text/html");
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "MESSAGE!!!");
+            return freeMarkerEngine.render(new ModelAndView(map, "home.ftl"));
+            });
         app();
     }
 
