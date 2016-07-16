86c1c2345a62edeaa0471924ed4002ecfca9340d
rkcerman <rkaliarik@gmail.com>
2016-07-16 18:21:22 +0200

    Added file upload.

--- src/main/java/com/rk/jsonvaluefetcher/JSONValueFetcher.java
+++ src/main/java/com/rk/jsonvaluefetcher/JSONValueFetcher.java
@@ -6,9 +6,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.Part;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 import static spark.Spark.*;
 
@@ -32,6 +36,48 @@ public class JSONValueFetcher extends Fetcher {
             map.put("message", "MESSAGE!!!");
             return freeMarkerEngine.render(new ModelAndView(map, "home.ftl"));
             });
+
+        post("/upload", "multipart/form-data", (request, response) -> {
+
+            String location = "/tmp";          // the directory location where files will
+            // be stored
+            long maxFileSize = 100000000;       // the maximum size allowed for
+            // uploaded files
+            long maxRequestSize = 100000000;    // the maximum size allowed for multipart/form-data requests
+            int fileSizeThreshold = 1024;       // the size threshold after which files will be written to disk
+
+            MultipartConfigElement multipartConfigElement = new MultipartConfigElement(
+                    location, maxFileSize, maxRequestSize, fileSizeThreshold);
+            request.raw().setAttribute("org.eclipse.jetty.multipartConfig",
+                    multipartConfigElement);
+
+            Collection<Part> parts = request.raw().getParts();
+            for (Part part : parts) {
+                System.out.println("Name: " + part.getName());
+                System.out.println("Size: " + part.getSize());
+                System.out.println("Filename: " + part.getSubmittedFileName());
+            }
+
+            String fName = request.raw().getPart("file").getSubmittedFileName();
+            UUID uuid = UUID.randomUUID();
+            fName += uuid.toString();
+
+            System.out.println("Title: " + request.raw().getParameter("title"));
+            System.out.println("File: " + fName);
+
+            Part uploadedFile = request.raw().getPart("file");
+            Path out = Paths.get("tmp/" + fName);
+            try (final InputStream in = uploadedFile.getInputStream()) {
+                Files.copy(in, out);
+                uploadedFile.delete();
+            }
+            multipartConfigElement = null;
+            parts = null;
+            uploadedFile = null;
+
+            return "OK";
+        });
+
         app();
     }
 
--- src/main/resources/home.ftl
+++ src/main/resources/home.ftl
@@ -1,8 +1,14 @@
-<head>[]
-  <title>Welcome!</title>[]
-</head>[]
-<body>[]
-  <#-- Greet the user with his/her name -->[]
-  <h1>Welcome ${message}!</h1>[]
-</body>[]
+<head>
+  <title>Welcome!</title>
+</head>
+<body>
+  <h1>Welcome ${message}!</h1>
+  <form action="/upload" enctype="multipart/form-data" method="post">
+	  <label for="title">Title</label>
+	  <input type="text" name="title"><br>
+	  <label for="file">File to send</label>
+	  <input type="file" name="file"><br>
+	  <input type="submit" value="Upload">
+  </form>
+</body>
 </html>
